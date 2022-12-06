05215fcabbbb67faa635804dc6b31fa79d645500
soledademaria <solmmaria50@gmail.com>
2022-12-05 21:11:00 -0300

    useState

--- ignite-feed/src/components/Comment.jsx
+++ ignite-feed/src/components/Comment.jsx
@@ -1,10 +1,10 @@
 import { ThumbsUp, Trash } from "phosphor-react";
 import { Avatar } from "./Avatar";
 import styles from "./Comment.module.css";
-export function Comment() {
+export function Comment({content}) {
   return (
     <div className={styles.comment}>
-      <Avatar />
+      <Avatar src="https://github.com/mariasoledadee.png" hasBorder={false}/>
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
@@ -19,7 +19,7 @@ export function Comment() {
             </button>
           </header>
 
-          <p>Muito bom</p>
+          <p>{content}</p>
         </div>
 
         <footer>
--- ignite-feed/src/components/Post.jsx
+++ ignite-feed/src/components/Post.jsx
@@ -5,12 +5,21 @@ import { format, formatDistanceToNow } from "date-fns";
 import { ptBR } from "date-fns/locale";
 
 import styles from "./Post.module.css";
+import { useState } from "react";
 
 export function Post({
   author: { name, role, avatarUrl },
   publishedAt,
   content,
 }) {
+  // estados
+  const [comments, setComments] = useState([
+    "Opa, testando aqui o meu primeiro comentário",
+  ]);
+
+  const [newCommentText, setNewCommentText] = useState("");
+
+  // variaveis
   const publishedDateFormatted = format(
     publishedAt,
     "d 'de' LLLL 'às' HH:mm'h'",
@@ -21,6 +30,22 @@ export function Post({
     locale: ptBR,
     addSuffix: true,
   });
+
+  // métodos
+  function handleCreateNewComment() {
+    event.preventDefault();
+
+    const newCommentText = event.target.input.value;
+
+    setComments([...comments, newCommentText]);
+    setNewCommentText('')
+
+  }
+
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value)
+  }
+
   return (
     <article className={styles.post}>
       <header className={styles.header}>
@@ -53,9 +78,14 @@ export function Post({
         })}
       </div>
 
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback!</strong>
-        <textarea placeholder="Deixe um comentário" />
+        <textarea
+          name="input"
+          value={newCommentText}
+          placeholder="Deixe um comentário"
+          onChange={handleNewCommentChange}
+        />
 
         <footer>
           <button type="submit">Publicar</button>
@@ -63,9 +93,9 @@ export function Post({
       </form>
 
       <div className={styles.commentList}>
-        <Comment/>
-        <Comment/>
-        <Comment/>
+        {comments.map((comment) => {
+          return <Comment content={comment} />;
+        })}
       </div>
     </article>
   );
